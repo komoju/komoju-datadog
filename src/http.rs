3e4a18f3d318eb352b131465d5267e6f7fc90bf6
Robin Schroer <git@sulami.xyz>
2025-09-16 13:32:54 +0900

    feat: Add a function to propagate tracing headers

--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,9 @@
 //! HTTP-related utilities
 
+use http::{HeaderMap, HeaderName, HeaderValue};
 use itertools::Itertools;
 use regex::Regex;
-use std::sync::LazyLock;
+use std::{str::FromStr, sync::LazyLock};
 
 /// Returns a Datadog-style path group from a request path, with dynamic segments replaced by '?'.
 ///
@@ -24,6 +25,51 @@ pub fn path_group(path: &str) -> String {
 /// Regular expression that matches static segments in request paths, e.g. "api" or "v1".
 static STATIC_SEGMENT_RE: LazyLock<Regex> =
     LazyLock::new(|| Regex::new("^(?:[^0-9]*|v[0-9]+)$").expect("invalid static segment regex"));
+
+/// Attaches tracing headers to a request's [`HeaderMap`], so that the far side can continue the
+/// current trace.
+///
+/// # Example
+///
+/// ```
+/// use komoju_datadog::http::attach_tracing_headers;
+///
+/// let mut request = http::Request::builder().body("").unwrap();
+/// attach_tracing_headers(request.headers_mut());
+/// ```
+pub fn attach_tracing_headers(headers: &mut HeaderMap) {
+    opentelemetry::global::get_text_map_propagator(|propagator| {
+        use tracing_opentelemetry::OpenTelemetrySpanExt;
+        let context = tracing::Span::current().context();
+        propagator.inject_context(&context, &mut HeaderCarrier::new(headers));
+    });
+}
+
+/// A wrapper for trait implementation, as we need to implement
+/// [`opentelemetry::propagation::Injector`] for [`HeaderMap`], neither of which we own.
+struct HeaderCarrier<'a> {
+    headers: &'a mut HeaderMap,
+}
+
+impl<'a> HeaderCarrier<'a> {
+    pub fn new(headers: &'a mut HeaderMap) -> Self {
+        HeaderCarrier { headers }
+    }
+}
+
+impl HeaderCarrier<'_> {
+    fn set_inner(&mut self, key: &str, value: String) {
+        let header_name = HeaderName::from_str(key).expect("Must be header name");
+        let header_value = HeaderValue::from_str(&value).expect("Must be a header value");
+        self.headers.insert(header_name, header_value);
+    }
+}
+
+impl opentelemetry::propagation::Injector for HeaderCarrier<'_> {
+    fn set(&mut self, key: &str, value: String) {
+        self.set_inner(key, value)
+    }
+}
 
 #[cfg(test)]
 mod tests {
