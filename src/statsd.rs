889af97688894505d58230def6e828d667cdd7c9
Robin Schroer <git@sulami.xyz>
2025-09-16 13:41:56 +0900

    feat: Improve ergonomics of the statsD client

--- src/statsd.rs
+++ src/statsd.rs
@@ -1,25 +1,87 @@
 //! StatsD metrics
 //!
-//! Metrics should use a single, shared, global client to submit metrics.
+//! This mostly proxies to the [`dogstatsd`] crate, which has more documentation on the API.
 
-use std::sync::OnceLock;
+use std::{ops::Deref, sync::OnceLock};
 
-/// Global DogstatsD client for submitting metrics.
-pub static METRICS: OnceLock<dogstatsd::Client> = OnceLock::new();
+/// Global StatsD instance, if used.
+static GLOBAL_STATSD: OnceLock<StatsD> = OnceLock::new();
 
-/// Initializes the global DogstatsD client.
-pub fn init(config: &crate::Config) {
-    let dogstatsd_client = dogstatsd::Client::new(
-        dogstatsd::OptionsBuilder::new()
-            .to_addr(config.metrics_agent_url.clone())
-            .default_tag(format!("service:{}", config.service))
-            .default_tag(format!("env:{}", config.env))
-            .default_tag(format!("version:{}", config.version))
-            .build(),
-    )
-    .expect("failed to create DogstatsD client");
+/// A client for submitting metrics to the DataDog agent.
+///
+/// Includes default tags for unified service tagging.
+///
+/// A single, global client is recommended, but a client can also be created and passed around
+/// where needed.
+///
+/// # Example
+///
+/// ```
+/// use komoju_datadog::statsd::StatsD;
+///
+/// let config = komoju_datadog::Config::builder().build();
+/// let statsd = StatsD::init_global(&config);
+///
+/// // From anywhere in the service.
+/// let _ = StatsD::global().incr("my_counter", &["tag:counter"]);
+/// ```
+pub struct StatsD {
+    inner: dogstatsd::Client,
+}
 
-    METRICS
-        .set(dogstatsd_client)
-        .expect("failed to set DogstatsD client");
+impl StatsD {
+    /// Creates a new StatsD client.
+    ///
+    /// Generally, one should use only one client for the entire service. Either pass it into
+    /// the right places or use a static [`OnceLock`] to make it accessible from anywhere.
+    /// See [`StatsD::init_global`] for a shortcut version.
+    ///
+    /// # Panics
+    ///
+    /// Can panic if bad options are passed.
+    pub fn new(config: &crate::Config) -> Self {
+        let inner = dogstatsd::Client::new(
+            dogstatsd::OptionsBuilder::new()
+                .to_addr(config.metrics_agent_url.clone())
+                .default_tag(format!("service:{}", config.service))
+                .default_tag(format!("env:{}", config.env))
+                .default_tag(format!("version:{}", config.version))
+                .build(),
+        )
+        .expect("failed to create DogstatsD client");
+
+        Self { inner }
+    }
+
+    /// Creates a new global StatsD client.
+    ///
+    /// # Panics
+    ///
+    /// Can panic if bad options are passed.
+    pub fn init_global(config: &crate::Config) -> &'static Self {
+        GLOBAL_STATSD.get_or_init(|| StatsD::new(config))
+    }
+
+    /// Gets the global StatsD client.
+    ///
+    /// # Panics
+    ///
+    /// Panics if [`StatsD::init_global`] has not been called. Use [`StatsD::try_global`] for a
+    /// non-panicking version.
+    pub fn global() -> &'static Self {
+        Self::try_global().expect("global StatsD client not initialized")
+    }
+
+    /// Attempts to get the global StatsD client.
+    pub fn try_global() -> Option<&'static Self> {
+        GLOBAL_STATSD.get()
+    }
+}
+
+impl Deref for StatsD {
+    type Target = dogstatsd::Client;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
 }
